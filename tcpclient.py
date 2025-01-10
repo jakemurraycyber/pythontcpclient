1863d7c0e3231fc4a9e0c5463e9a17f01454270e
Jake <jake.murray9010@gmail.com>
2025-01-10 17:10:08 -0600

    Added python.org to program for testing

--- tcpclient.py
+++ tcpclient.py
@@ -1,7 +1,7 @@
 import socket
 
-target_host = '****' # add target URL here
-target_port = 000 # add target port here
+target_host = 'www.python.org' # add target URL here
+target_port = 443 # add target port here
 
 # Create socket object
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -10,7 +10,7 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((target_host, target_port))
 
 # Send some data
-client.send(b'GET / HTTP/1.1\r\nHOST: google.com\r\n\r\n')
+client.send(b'GET / HTTP/1.1\r\nHOST: python.org\r\n\r\n')
 
 # Receive some data
 response = client.recv(4096)
